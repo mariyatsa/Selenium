10159844137aa50b82b26842281f17efcc1d19f7
Maryia <tsapkova_mariya@bk.ru>
2023-08-01 18:25:34 +0500

    #Test3

--- src/test/java/KartaDedetovayaTest.java
+++ src/test/java/KartaDedetovayaTest.java
@@ -1,5 +1,4 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import junit.framework.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,6 +9,8 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class KartaDedetovayaTest {
     private static WebDriver driver;
@@ -34,13 +35,13 @@ public class KartaDedetovayaTest {
     void test1() throws InterruptedException {
         // загружаем страницу
         driver.get("http://http://localhost:9999/");
-       // WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
-        //form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Mariya");
-        //form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79200005544");
-        //form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        //form.findElement(By.cssSelector("[data-test-id=submit]")).click();
-        //String text = driver.findElement(By.className("alert-success")).getText();
-        //Assert.assertEquals(" Ваша заявка успешно отправлена!", text);
+       WebElement form = driver.findElement(By.cssSelector("[data-test-id=callback-form]"));
+       form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Mariya");
+       form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79200005544");
+       form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+       form.findElement(By.cssSelector("[data-test-id=submit]")).click();
+       String text = driver.findElement(By.className("alert-success")).getText();
+       assertEquals(" Ваша заявка успешно отправлена!", text);
 
         // задержка открываемой страницы
         Thread.sleep(5000);
